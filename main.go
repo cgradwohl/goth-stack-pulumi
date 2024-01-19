66349747aae032832d43aa968fd0ac7ed19ebf36
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-01-19 06:38:37 -0800

    ecr and docker image IAC

--- main.go
+++ main.go
@@ -1,20 +1,63 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
+	"fmt"
+
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ecr"
+	"github.com/pulumi/pulumi-docker/sdk/v4/go/docker"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
+		// Create ECR repository
+		ecrRepository, err := ecr.NewRepository(ctx, "goth-stack-repo", &ecr.RepositoryArgs{
+			Name: pulumi.String("goth-docker-repository"),
+			ImageScanningConfiguration: &ecr.RepositoryImageScanningConfigurationArgs{
+				ScanOnPush: pulumi.Bool(true),
+			},
+		})
 		if err != nil {
 			return err
 		}
 
-		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
+		// Create an auth token for the ECR repository
+		authToken := ecr.GetAuthorizationTokenOutput(ctx, ecr.GetAuthorizationTokenOutputArgs{
+			RegistryId: ecrRepository.RegistryId,
+		}, nil)
+
+		// Build the Docker image from local files
+		myAppImage, err := docker.NewImage(ctx, "my-app-image", &docker.ImageArgs{
+			Build: &docker.DockerBuildArgs{
+				Args: pulumi.StringMap{
+					"BUILDKIT_INLINE_CACHE": pulumi.String("1"),
+				},
+				CacheFrom: &docker.CacheFromArgs{
+					Images: pulumi.StringArray{
+						ecrRepository.RepositoryUrl.ApplyT(func(repositoryUrl string) (string, error) {
+							return fmt.Sprintf("%v:latest", repositoryUrl), nil
+						}).(pulumi.StringOutput),
+					},
+				},
+				Context:    pulumi.String("app/"),
+				Dockerfile: pulumi.String("Dockerfile"),
+			},
+			ImageName: ecrRepository.RepositoryUrl.ApplyT(func(repositoryUrl string) (string, error) {
+				return fmt.Sprintf("%v:latest", repositoryUrl), nil
+			}).(pulumi.StringOutput),
+			Registry: &docker.RegistryArgs{
+				Password: pulumi.ToSecret(authToken.ApplyT(func(authToken ecr.GetAuthorizationTokenResult) (*string, error) {
+					return &authToken.Password, nil
+				}).(pulumi.StringPtrOutput)).(*pulumi.StringOutput),
+				Server: ecrRepository.RepositoryUrl,
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Pulumi Exports
+		ctx.Export("imageName", myAppImage.ImageName)
 		return nil
 	})
 }
